3832a8ab2d9267c1c6afa74e412493053e04dae4
luoyu <2085324623@qq.com>
2025-05-28 22:02:43 +0800

    修改了图片上传保存的地址

--- src/main/java/com/hmdp/utils/SystemConstants.java
+++ src/main/java/com/hmdp/utils/SystemConstants.java
@@ -1,7 +1,8 @@
 package com.hmdp.utils;
 
 public class SystemConstants {
-    public static final String IMAGE_UPLOAD_DIR = "/Users/gaojianheng/Desktop/nginx/hmdp/imgs/";
+    // 笔记图片保存地址
+    public static final String IMAGE_UPLOAD_DIR = "E:/hmd/nginx/hmdp/imgs/";;
     public static final String USER_NICK_NAME_PREFIX = "user_";
     public static final String USER_CODE = "code";
     public static final int DEFAULT_PAGE_SIZE = 5;
